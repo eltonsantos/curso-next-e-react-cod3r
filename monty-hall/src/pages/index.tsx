5d11c8d61108401557e27ffa24d8e1e97bbaa258
Elton Santos <eltonaxl@hotmail.com>
2023-06-23 14:12:43 -0300

    Change home

--- monty-hall/src/pages/index.tsx
+++ monty-hall/src/pages/index.tsx
@@ -1,9 +1,43 @@
-import Gift from "../components/Gift";
+import { useState } from "react";
+import Card from "../components/Card";
+import InputNumeric from "../components/InputNumeric";
+import Link from "next/link";
+import styles from "../styles/Form.module.css";
 
-export default function Home() {
+export default function Form() {
+  const [qtdDoors, setQtdDoors] = useState(3);
+  const [withGift, setWithGift] = useState(1);
+
   return (
-    <>
-      <Gift />
-    </>
-  )
+    <div className={styles.form}>
+      <div>
+        <Card bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card>
+          <InputNumeric
+            text="Qtde Portas?"
+            value={qtdDoors}
+            onChange={(newQtd) => setQtdDoors(newQtd)}
+          />
+        </Card>
+      </div>
+      <div>
+        <Card>
+          <InputNumeric
+            text="Porta com Presente?"
+            value={withGift}
+            onChange={(newDoorWithGift) =>
+              setWithGift(newDoorWithGift)
+            }
+          />
+        </Card>
+        <Card bgcolor="#28a085">
+          <Link href={`/game/${qtdDoors}/${withGift}`} passHref>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
+    </div>
+  );
 }
