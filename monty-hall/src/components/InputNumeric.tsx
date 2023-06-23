5d549daeb6109bb6348f8994df2353a77310867f
Elton Santos <eltonaxl@hotmail.com>
2023-06-23 14:04:26 -0300

    Create components Card and InputNumeric

---
+++ monty-hall/src/components/InputNumeric.tsx
@@ -0,0 +1,27 @@
+import styles from "../styles/InputNumeric.module.css";
+
+interface InputNumericProps {
+  text: string;
+  value: number;
+  onChange: (newValue: number) => void;
+}
+
+export default function InputNumeric(props: InputNumericProps) {
+  const dec = () => props.onChange(props.value - 1);
+  const inc = () => props.onChange(props.value + 1);
+
+  return (
+    <div className={styles.inputNumeric}>
+      <span className={styles.text}>{props.text}</span>
+      <span className={styles.value}>{props.value}</span>
+      <div className={styles.buttons}>
+        <button className={styles.btn} onClick={dec}>
+          -
+        </button>
+        <button className={styles.btn} onClick={inc}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
