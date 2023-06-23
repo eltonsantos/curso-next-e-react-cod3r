faafeb0db2b93ade430ccb1792f5acae096c2a25
Elton Santos <eltonaxl@hotmail.com>
2023-06-23 11:10:30 -0300

    Create class Door

---
+++ monty-hall/src/model/door.js
@@ -0,0 +1,48 @@
+export default class DoorModel {
+  #number
+  #hasGift
+  #selected
+  #open
+
+  constructor(number, hasGift = false, selected = false, open = false) {
+      this.#number = number
+      this.#hasGift = hasGift
+      this.#selected = selected
+      this.#open = open
+  }
+
+  get number() {
+      return this.#number
+  }
+
+  get hasGift() {
+      return this.#hasGift
+  }
+
+  get selected() {
+      return this.#selected
+  }
+
+  get opened() {
+      return this.#open
+  }
+
+  get closed() {
+      return !this.open
+  }
+
+  unSelect() {
+      const selected = false
+      return new DoorModel(this.number, this.hasGift, selected, this.opened)        
+  }
+
+  changeSelection() {
+      const selected = !this.selected
+      return new DoorModel(this.number, this.hasGift, selected, this.opened)
+  }
+
+  open() {
+      const opened = true
+      return new DoorModel(this.number, this.hasGift, this.selected, opened)
+  }
+}
