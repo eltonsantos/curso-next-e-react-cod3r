f52b2125f938926202f2f9521831f70680c5f069
Elton Santos <eltonaxl@hotmail.com>
2023-06-23 02:00:00 -0300

    Create component Gift

---
+++ monty-hall/src/components/Gift.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Gift.module.css"
+
+export default function Gift() {
+  return (
+    <div className={styles.box}>
+      <div className={styles.cover}></div>
+      <div className={styles.container}></div>
+      <div className={styles.ribbon1}></div>
+      <div className={styles.ribbon2}></div>
+    </div>
+  )
+}
