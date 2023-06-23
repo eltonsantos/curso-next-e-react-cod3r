5f2cca14a35e9b78397ae831cd89c183f47f5b12
Elton Santos <eltonaxl@hotmail.com>
2023-06-23 02:25:44 -0300

    Add more things to door

--- monty-hall/src/components/Door.jsx
+++ monty-hall/src/components/Door.jsx
@@ -1,10 +1,31 @@
-import styles from "../styles/Door.module.css"
+import styles from "../styles/Door.module.css";
+import Gift from "./Gift";
 
 export default function Door() {
+  const door = props.value;
+  const selected = door.selected && !door.opened ? styles.selected : "";
+
+  const changeSelection = (e) => props.onChange(door.changeSelection());
+  const open = (e) => {
+    e.stopPropagation();
+    props.onChange(door.open());
+  };
+
+  function renderDoor() {
+    return (
+      <div className={styles.door}>
+        <div className={styles.number}>{door.number}</div>
+        <div className={styles.knob} onClick={open}></div>
+      </div>
+    );
+  }
 
   return (
-    <div className={styles.area}>
+    <div className={styles.area} onClick={changeSelection}>
+      <div className={`${styles.structure} ${selected}`}>
+        {door.closed ? renderDoor() : door.haveGift ? <Gift /> : false}
+      </div>
       <div className={styles.floor}></div>
     </div>
-  )
+  );
 }
