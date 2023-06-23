9ac53c3e0f80eff0a584ecf15c9c5bf47cd54b67
Elton Santos <eltonaxl@hotmail.com>
2023-06-23 02:11:10 -0300

    Change css and create component door

---
+++ monty-hall/src/components/Door.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Door.module.css"
+
+export default function Door() {
+
+    return (
+      <div className={styles.area}>
+        <div className={styles.floor}></div>
+      </div>
+  )
+}
