23345512b9fbe3beab11f2a6efa1414bad7431f4
Elton Santos <eltonaxl@hotmail.com>
2023-07-14 05:28:32 -0300

    Change base_url to deploy

--- quiz/src/pages/index.tsx
+++ quiz/src/pages/index.tsx
@@ -3,7 +3,9 @@ import QuestionModel from "@/model/question";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-const BASE_URL = 'http://localhost:3000/api'
+// const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'https://quiz-eltonsantos.vercel.app/api'
+
 
 export default function Home() {
   const router = useRouter()
