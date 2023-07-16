7a2512b3844cd4e74463bc1612d1efc837a701e7
Elton Santos <eltonaxl@hotmail.com>
2023-07-16 03:27:25 -0300

    Create component SidebarMenu

--- admin-template/src/components/template/MenuItem.tsx
+++ admin-template/src/components/template/MenuItem.tsx
@@ -1,22 +1,35 @@
 import Link from "next/link";
 
 interface MenuItemProps {
-  url: string;
   text: string;
   icon: any;
+  url?: string;
+  className?: string;
+  onClick?: (event: any) => void;
 }
 
 export default function SidebarMenu(props: MenuItemProps) {
+
+  function renderLink() {
+    return (
+      <a className={`flex flex-col justify-center items-center h-20 w-20 text-gray-600 ${props.className}`}>
+        {props.icon}
+        <span className="text-xs font-light">
+          {props.text}
+        </span>
+      </a>
+    )
+  }
+  
   return (
-    <li className="hover:bg-gray-100">
-      <Link href={props.url} legacyBehavior>
-        <a className="flex flex-col justify-center items-center h-20 w-20">
-          {props.icon}
-          <span className="text-xs font-light text-gray-600">
-            {props.text}
-          </span>
-        </a>
-      </Link>
+    <li onClick={props.onClick} className="hover:bg-gray-100 cursor-pointer">
+      {props.url ? (
+        <Link href={props.url} legacyBehavior>
+          {renderLink()}
+        </Link>
+      ) : (
+        renderLink()
+      )}
     </li>
   )
 }
--- admin-template/src/components/template/SidebarMenu.tsx
+++ admin-template/src/components/template/SidebarMenu.tsx
@@ -1,17 +1,20 @@
-import { BellIcon, HomeIcon, SettingsIcon } from "../icons";
+import { BellIcon, HomeIcon, LogoutIcon, SettingsIcon } from "../icons";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function SidebarMenu() {
   return (
-    <aside>
+    <aside className="flex flex-col">
       <div className="flex flex-col items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800">
         <Logo />
       </div>
-      <ul>
+      <ul className="flex-grow">
         <MenuItem url="/" text="Início" icon={HomeIcon} />
         <MenuItem url="/settings" text="Ajustes" icon={SettingsIcon} />
         <MenuItem url="/notifications" text="Notificações" icon={BellIcon} />
+      </ul>
+      <ul className="">
+        <MenuItem onClick={() => console.log("Logout")} text="Sair" icon={LogoutIcon} className="text-red-600 hover:bg-red-400 hover:text-white" />
       </ul>
     </aside>
   )
