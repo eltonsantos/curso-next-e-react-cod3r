53653586e327fee1e7d897f33631370295a23e1d
Elton Santos <eltonaxl@hotmail.com>
2023-07-16 02:28:59 -0300

    Create component Layout

--- admin-template/src/components/template/Layout.tsx
+++ admin-template/src/components/template/Layout.tsx
@@ -1,6 +1,6 @@
-import Content from "./Content";
-import Header from "./Header";
 import SidebarMenu from "./SidebarMenu";
+import Header from "./Header";
+import Content from "./Content";
 
 interface LayoutProps {
   title: string;
@@ -10,12 +10,14 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   return (
-    <div>
+    <div className="flex h-screen w-screen">
       <SidebarMenu />
-      <Header title={props.title} subtitle={props.subtitle} />
-      <Content>
-        {props.children}
-      </Content>
+      <div className="flex flex-col w-full p-7 bg-gray-300">
+        <Header title={props.title} subtitle={props.subtitle} />
+        <Content>
+          {props.children}
+        </Content>
+      </div>
     </div>
   )
 }
