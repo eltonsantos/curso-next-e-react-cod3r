ab97500fddd28360ea219543517c341fb2c0e472
Elton Santos <eltonaxl@hotmail.com>
2023-07-16 03:53:30 -0300

    Color scheme 2

--- admin-template/src/components/template/MenuItem.tsx
+++ admin-template/src/components/template/MenuItem.tsx
@@ -12,7 +12,7 @@ export default function SidebarMenu(props: MenuItemProps) {
 
   function renderLink() {
     return (
-      <a className={`flex flex-col justify-center items-center h-20 w-20 text-gray-600 ${props.className}`}>
+      <a className={`flex flex-col justify-center items-center h-20 w-20 dark:text-gray-200 ${props.className}`}>
         {props.icon}
         <span className="text-xs font-light">
           {props.text}
@@ -22,7 +22,7 @@ export default function SidebarMenu(props: MenuItemProps) {
   }
   
   return (
-    <li onClick={props.onClick} className="hover:bg-gray-100 cursor-pointer">
+    <li onClick={props.onClick} className="hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
       {props.url ? (
         <Link href={props.url} legacyBehavior>
           {renderLink()}
--- admin-template/src/components/template/SidebarMenu.tsx
+++ admin-template/src/components/template/SidebarMenu.tsx
@@ -4,7 +4,7 @@ import MenuItem from "./MenuItem";
 
 export default function SidebarMenu() {
   return (
-    <aside className="flex flex-col">
+    <aside className="flex flex-col dark:bg-gray-900 bg-gray-200 text-gray-700">
       <div className="flex flex-col items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800">
         <Logo />
       </div>
@@ -14,7 +14,7 @@ export default function SidebarMenu() {
         <MenuItem url="/notifications" text="Notificações" icon={BellIcon} />
       </ul>
       <ul className="">
-        <MenuItem onClick={() => console.log("Logout")} text="Sair" icon={LogoutIcon} className="text-red-600 hover:bg-red-400 hover:text-white" />
+        <MenuItem onClick={() => console.log("Logout")} text="Sair" icon={LogoutIcon} className="text-red-600 dark:text-red-400 hover:bg-red-400 hover:text-white dark:hover:text-white" />
       </ul>
     </aside>
   )
