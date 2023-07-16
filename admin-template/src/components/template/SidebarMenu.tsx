08d780967ddaeff8135815fc29ef20a034be4e35
Elton Santos <eltonaxl@hotmail.com>
2023-07-16 03:10:21 -0300

    Create component Logo

--- admin-template/src/components/template/SidebarMenu.tsx
+++ admin-template/src/components/template/SidebarMenu.tsx
@@ -1,9 +1,13 @@
 import { BellIcon, HomeIcon, SettingsIcon } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function SidebarMenu() {
   return (
     <aside>
+      <div className="flex flex-col items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800">
+        <Logo />
+      </div>
       <ul>
         <MenuItem url="/" text="Início" icon={HomeIcon} />
         <MenuItem url="/settings" text="Ajustes" icon={SettingsIcon} />
