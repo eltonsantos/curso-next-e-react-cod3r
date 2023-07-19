35257922cd03193b16b5bc6974c2db34d6f6bb9c
Elton Santos <eltonaxl@hotmail.com>
2023-07-18 23:36:29 -0300

    Create component Input, Form and hooks

--- next-crud-advanced/src/components/Form.tsx
+++ next-crud-advanced/src/components/Form.tsx
@@ -1,4 +1,7 @@
-import Customer from "@/core/Customer"
+import { useState } from "react";
+import Customer from "../core/Customer";
+import Button from "./Button";
+import Input from "./Input";
 
 interface FormProps {
   customer: Customer
@@ -7,7 +10,41 @@ interface FormProps {
 }
 
 export default function Form(props: FormProps) {
+  const id = props.customer?.id
+  const [name, setName] = useState(props.customer?.name ?? '')
+  const [age, setAge] = useState(props.customer?.age ?? 0)
+
   return (
-    <div></div>
+    <div>
+      {id ? (
+        <Input
+          readonly
+          text="Código"
+          value={id}
+          className="mb-5"
+        />
+      ) : false}
+      <Input
+        text="Nome"
+        value={name}
+        changedValue={setName}
+        className="mb-5"
+      />
+      <Input
+        text="Idade"
+        type="number"
+        value={age}
+        changedValue={setAge}
+      />
+      <div className="flex justify-end mt-7">
+        <Button color="blue" className="mr-2"
+          onClick={() => props.changeCustomer?.(new Customer(name, +age, id))}>
+          {id ? 'Alterar' : 'Salvar'}
+        </Button>
+        <Button onClick={props.canceled}>
+          Cancelar
+        </Button>
+      </div>
+    </div>
   )
 }
